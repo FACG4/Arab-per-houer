d27db2d3fd22b7c657215a383f69f44828be1997
bnazmi <6nazmi@gmail.com>
2018-05-24 14:10:02 +0300

    login form

--- public/js/dom.js
+++ public/js/dom.js
@@ -26,28 +26,12 @@ select('#close-btn-project').addEventListener('click',()=>{
     select('.project--details-pop').classList.toggle('hidden');
 })
 
-   var modal = select('#myModal');
-   var btn = select('#myBtn');
-   var span = select('.close');
-   var msg= select('.msg');
 
-   btn.onclick = function() {
-       modal.style.display = "block";
-   }
-   
-   span.onclick = function() {
-       modal.style.display = "none";
-   }
-   window.onclick = function(event) {
-       if (event.target == modal) {
-           modal.style.display = "none";
-       }
-   }
   
-   const btnn= select(".buttonS");
-   btnn.addEventListener("click", function(){
-   const name = select("#name").value;
-   const password = select("#password").value;
+//    const btnn= select(".buttonS");
+//    btnn.addEventListener("click", function(){
+//    const name = select("#name").value;
+//    const password = select("#password").value;
   
    fetch('/','POST' ,name, password, function(res){
      msg.textContent=res;
